4b9d4c14ad6a534cd5d4d3904947828f078d145b
Kyle Karhohs <gandalfisarockstar@gmail.com>
2011-11-15 13:32:07 -0500

    created an XML configuration file

---
+++ config.m
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="utf-8"?>
+<p53-Cinema_config>
+<path_to_metamorph_images>
+
+</path_to_metamorph_images>
+<path_to_flatfield_images>
+
+</path_to_flatfield_images>
+<path_to_
+
+</path_to_
+<objective_magnification></objective_magnification>
+<binning></binning>
+<wavelength>
+    <number>1</number>
+    <label></label>
+</wavelength>
+<wavelength>
+    <number>2</number>
+    <label></label>
+</wavelength>
+<wavelength>
+    <number>3</number>
+    <label></label>
+</wavelength>
+<wavelength>
+    <number>4</number>
+    <label></label>
+</wavelength>
+<wavelength>
+    <number>5</number>
+    <label></label>
+</wavelength>
+</p53-Cinema_config>
