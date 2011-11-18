bded639c721894b5953dbc47291aad4a4e61754e
Kyle Karhohs <gandalfisarockstar@gmail.com>
2011-11-18 15:30:04 -0500

    The file is now almost fully operational. There exists a bug where the new-line character in any textnode crashes the program b/c it results in a bad char declaration. This is b/c of the extensive use of eval().

--- my_parseXML.m
+++ my_parseXML.m
@@ -19,7 +19,7 @@ end
 
 %Initialize S
 root_node = xdoc.getDocumentElement; %Locates the node at the root of the XML tag tree
-my_temp_name = regexprep(root_node.getNodeName,'[-:.]','_'); %Struct names cannot contain [-:.]
+my_temp_name = regexprep(root_node.getNodeName.toString.toCharArray','[-:.]','_'); %Struct names cannot contain [-:.]
 my_tree = ['S.' my_temp_name]; %The location in S where data is currently being created
 if root_node.hasAttributes
     my_attributes = root_node.getAttributes;
@@ -29,7 +29,7 @@ if root_node.hasAttributes
         my_temp_name = regexp(my_temp_string,'.*(?==)','match');
         my_temp_name = regexprep(my_temp_name,'[-:.]','_');
         my_temp_attribute = regexp(my_temp_string,'(?<=").*(?=")','match');
-        eval([my_tree '.attd8a.' my_temp_name '=' my_temp_attribute]);
+        eval([my_tree '.attd8a.' my_temp_name{1} '=''' my_temp_attribute{1} ''';'])
     end
 end
 
@@ -40,7 +40,7 @@ if root_node.hasChildNodes
             case 1
                 %NodeType 1 = element
                 S  = parseElement(current_node,S,my_tree);
-            case [3,4,8]
+            case {3,4,8}
                 %NodeType 3 = text node
                 %NodeType 4 = CDATA
                 %NodeType 8 = comment node
@@ -49,8 +49,6 @@ if root_node.hasChildNodes
         end
         current_node = current_node.getNextSibling;
     end
-    
-    
 end
 end
 
@@ -63,7 +61,7 @@ for i = 0:(my_length-1)
     my_temp_name = regexp(my_temp_string,'.*(?==)','match');
     my_temp_name = regexprep(my_temp_name,'[-:.]','_');
     my_temp_attribute = regexp(my_temp_string,'(?<=").*(?=")','match');
-    eval([tree '.attd8a.' my_temp_name '=''' my_temp_attribute ''';']);
+    eval([tree '.attd8a.' my_temp_name{1} '=''' my_temp_attribute{1} ''';']);
 end
 end
 
@@ -71,46 +69,58 @@ end
 function S = parseElement(node,S,tree)
 %WARNING: This is a recursive function and the author of this code does not
 %know if this could lead to an infinite loop or crashing MATLAB.
-my_temp_name = regexprep(node.getNodeName,'[-:.]','_'); %Struct names cannot contain [-:.]
-if isfield()
-    tree = [tree '.' my_temp_name '(end+1)']; %The location in S where data is currently being created
+my_temp_name = regexprep(node.getNodeName.toString.toCharArray','[-:.]','_'); %Struct names cannot contain [-:.]
+if eval(['isfield(' tree ',''' my_temp_name ''')'])
+    my_temp_tree = [tree '.' my_temp_name '(end+1)']; %The location in S where data is currently being created
+    eval([my_temp_tree '.txtd8a={};'])
+    tree = [tree '.' my_temp_name '(end)']; %The location in S where data is currently being created
 else
-        tree = [tree '.' my_temp_name '(1)']; %The location in S where data is currently being created
+    tree = [tree '.' my_temp_name '(1)']; %The location in S where data is currently being created
+    eval([tree '.txtd8a={};'])
 end
-
-
 if node.hasAttributes
     S = parseAttributes(node,S,tree);
 end
 if node.hasChildNodes
-    current_node = root_node.getFirstChild;
+    current_node = node.getFirstChild;
     while ~isempty(current_node)
-        switch node.getNodeType
+        switch current_node.getNodeType
             case 1
                 %NodeType 1 = element
-                S  = parseElement(current_node,S,my_tree);
-            case [3,4,8]
+                S  = parseElement(current_node,S,tree);
+            case {3,4,8}
                 %NodeType 3 = text node
                 %NodeType 4 = CDATA
                 %NodeType 8 = comment node
-                S = parseMiscellaneous(current_node,S);
+                S = parseMiscellaneous(current_node,S,tree);
             otherwise
         end
         current_node = current_node.getNextSibling;
     end
 else
-    my_temp_string = node.getTextContent.toString.toCharArrary';
-    eval([tree '=''' my_temp_string ''';'])
+    my_temp_string = node.getTextContent.toString.toCharArray';
+    eval([tree '.txtd8a{1}=''' my_temp_string ''';'])
 end
 end
 
 % ----- Subfunction PARSEMISCELLANEOUS -----
 function S = parseMiscellaneous(node,S,tree)
-try
-    my_temp_string = node.getData.toString.toCharArray';
-    eval([tree '.txtd8a{end+1}=''' my_temp_string ''';'])
-catch %#ok<CTCH>
-    my_temp_string = node.getData.toString.toCharArray';
-    eval([tree '.txtd8a{1}=''' my_temp_string ''';'])
+my_temp_string = node.getData.toString.toCharArray';
+flag = checkString(my_temp_string);
+if flag
+    if eval(['isfield(' tree ',''txtd8a'')'])
+        eval([tree '.txtd8a{end+1}=''' my_temp_string ''';'])
+    else
+        eval([tree '.txtd8a{1}=''' my_temp_string ''';'])
+    end
+end
+
+end
+
+% ----- Subfunction CHECKSTRING -----
+function [flag] = checkString(my_str)
+flag = 1;
+if ~isempty(strfind(my_str,sprintf('\n')))
+    flag = 0;
 end
 end
