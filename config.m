b395e4db9d5d16383d0ed1ac04549bd4461cf45b
Kyle Karhohs <gandalfisarockstar@gmail.com>
2011-11-17 22:53:14 -0500

    expanded the config file to contain dummy data to test possible bugs.

--- config.m
+++ config.m
@@ -1,15 +1,19 @@
 <?xml version="1.0" encoding="utf-8"?>
 <p53-Cinema_config att1="1" att2="2" att3="hi">
 <!-- This is a configuration file -->
+aaaaaaa
 <path_to_metamorph_images>
 kjashjjds
 </path_to_metamorph_images>
+bbbbbbb
 <path_to_flatfield_images>
 oijsdjdnv
 </path_to_flatfield_images>
+ccccccccc
 <path_to_eldorado>
 iwenwfo
 </path_to_eldorado>
+dddddd
 <objective_magnification></objective_magnification>
 <binning></binning>
 <wavelength>
@@ -17,6 +21,7 @@ iwenwfo
     <label></label>
     <exposure units="ms"></exposure>
 </wavelength>
+eeeeee
 <wavelength>
     <number>2</number>
     <label></label>
