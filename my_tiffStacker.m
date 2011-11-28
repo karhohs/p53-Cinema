b8439f8d0432d9ac4cd062c6e5bef98f4cc230f6
Kyle Karhohs <gandalfisarockstar@gmail.com>
2011-11-28 18:00:04 -0500

    removed the scaling function from 12-bit to 16-bit to add this function to a separate file.

--- my_tiffStacker.m
+++ my_tiffStacker.m
@@ -124,7 +124,6 @@ for i=1:Wmax
                 newnames{i,j} = Name;
                 t = Tiff(name2read,'r');
                 IM = t.read;
-                IM = scale12to16bit(IM);
                 timelabels{k} = p53TiffMetaAnalysis4Metamorph(t);
                 t.close;
                 imwrite(IM,Name,'tif','WriteMode','append','Compression','none');
@@ -186,16 +185,4 @@ fclose(fid);
 t.setTag('ImageDescription',F);
 t.rewriteDirectory;
 delete('t3mp.xml');
-end
-
-%----- SUBFUNCTION SCALE12TO16BIT -----
-function [IM] = scale12to16bit(IM)
-%The Hamamatsu cameras for the closet scope and curtain scope create images
-%with 12-bit dynamic range. However, the TIFF format that stores these
-%images uses a 16-bit format. Viewing a 12-bit image in a 16-bit format on
-%a computer monitor is complicated by the scaling being done at 16-bit. To
-%make viewing images from the microscope easier on a computer monitor,
-%without any compression or loss of data, the 12-bit data is shifted left
-%4-bits to become 16-bit data.
-IM = bitshift(IM,4);
 end
