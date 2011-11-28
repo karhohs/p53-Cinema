d5032296b78df9be4877c4ee8484262424453820
Kyle Karhohs <gandalfisarockstar@gmail.com>
2011-11-28 09:13:51 -0500

    added times of image acquisition. Currently debugging

--- my_tiffStacker.m
+++ my_tiffStacker.m
@@ -69,12 +69,12 @@ for i = 1:length(dirCon)
 end
 
 % ----- process optional arguments -----
-if isempty(positions)
+if ~exist('positions','var')
     positions = 1:Smax;
 elseif min(size(positions)) > 1 || max(size(positions)) > Smax
     error('the positions vector is an invalid size')
 end
-if isempty(timepoints)
+if ~exist('timepoints','var')
     timepoints = cell(1,Smax);
     temp = 1:Tmax;
     for i=1:Smax
@@ -96,6 +96,8 @@ mkdir(path2,'Stacks') %a new directory is generated for the stacks
 %loaded and appendend to a new (multi-layer) tif file named with '_tSTACK'
 
 % ----- Create stacks -----
+timelabels = cell(Tmax,Smax,Wmax); %This cell will contain the times images were acquired
+newnames = cell(Smax,Wmax);
 for i=1:Wmax
     for j=positions
         for k=timepoints{j}
@@ -119,11 +121,16 @@ for i=1:Wmax
                 %appends the image to the tif file using no compression
                 %Loading the images is the most time intensive part of the code
                 name2read = [path,'\',FileNames{k,j,1}];
+                newnames{j,i} = Name;
                 t = Tiff(name2read,'r');
                 IM = t.read;
-                p53TiffMetaAnalysis(t);
+                timelabels{i,j,k} = p53TiffMetaAnalysis4Metamorph(t);
+                t.close;
                 imwrite(IM,Name,'tif','WriteMode','append','Compression','none');
             end
+            t = Tiff(newnames{j,i},'w');
+            addTime2Stack(timelabels{i,j,:},t);
+            t.close;
         end
         fprintf(1,'.'); %shows some activity to calm user... (cheap progress bar)
     end
@@ -131,7 +138,48 @@ end
 fprintf(1,'\nMake Stacks Done!\n');
 end
 
-
+%----- SUBFUNCTION ADDTIME2STACK -----
+function [] = addTime2Stack(timelabels,t)
+%check for existing XML information
+try
+    metadata = t.getTag('ImageDescription');
+    fid = fopen('t3mp.xml','w');
+    fprintf(fid,'%s',metadata);
+    fclose(fid);
+    xdoc = xmlread('.t3mp.xml');
+    node_root = xdoc.getDocumentElement;
+    my_list = node_root.getElementsByTagName('time');
+    aprioritimelabels = my_list.item(0).getTextContent;
+    aprioritimelabels = regexp(aprioritimelabels,'.*(?=,)','match');
+    timelabels = vertcat(aprioritimelabels,timelabels);
+    timestr = timelabels{1};
+    for i=2:length(timelabels)
+        timestr = [timestr ',' timelabels{i}]; %#ok<AGROW>
+    end
+    my_list.item(0).setTextContent(timestr);
+    xmlwrite('t3mp.xml',xdoc);
+catch err %#ok<NASGU>
+    timestr = timelabels{1};
+    for i=2:length(timelabels)
+        timestr = [timestr ',' timelabels{i}]; %#ok<AGROW>
+    end
+    %----- Create New XML file -----
+    fid = fopen('t3mp.xml','w');
+    fprintf(fid,'<MetaData></MetaData>');
+    fclose(fid);
+    xdoc = xmlread('.t3mp.xml');
+    node_root = xdoc.getDocumentElement;
+    thisElement = xdoc.createElement('time');
+    thisElement.setTextContent(timestr);
+    node_root.appendChild(thisElement);
+    xmlwrite('t3mp.xml',xdoc);   
+end
+%append XML information to image description
+fid = fopen('t3mp.xml');
+F = fread(fid, '*char')';
+fclose(fid);
+t.setTag('ImageDescription',F);
+end
 
 
 
