b71e22ed4cd675f24e0295e900f6de22ad7994a5
Kyle Karhohs <gandalfisarockstar@gmail.com>
2011-11-30 19:26:22 -0500

    first commit

---
+++ flatfieldcorrection.m
@@ -0,0 +1,18 @@
+function []=flatfieldcorrection(stackpath,ffpath)
+% [] = my_tiffStacker(path,positions,timepoints)
+% Input:
+% stackpath: the location of TIFF that will be flat field corrected
+% ffpath: the location of the correction images 
+%
+% Output:
+% There is no direct argument output. Rather, new stacks will be created from
+% the stacks found in the stackpath directory and stored in a folder called
+% [stackpath '_ff'].
+%
+% Description:
+% 
+%
+% Other Notes:
+% 
+
+end
