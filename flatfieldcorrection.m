9d338180805a7281b7e5568615d6219807f647fd
Kyle Karhohs <gandalfisarockstar@gmail.com>
2011-11-30 23:28:15 -0500

    unfinished

--- flatfieldcorrection.m
+++ flatfieldcorrection.m
@@ -1,5 +1,5 @@
 function []=flatfieldcorrection(stackpath,ffpath)
-% [] = my_tiffStacker(path,positions,timepoints)
+% [] = flatfieldcorrection(stackpath,ffpath)
 % Input:
 % stackpath: the location of TIFF that will be flat field corrected
 % ffpath: the location of the correction images 
@@ -14,5 +14,32 @@ function []=flatfieldcorrection(stackpath,ffpath)
 %
 % Other Notes:
 % 
-
+if nargin==0
+    stackpath = 'C:\Users\Kyle\Documents\p53CinemaSTACKS';
+    ffpath='G:\KWKDocuments\My Dropbox\p53Cinema\flatfield_20110909';
 end
+%----- Import stacknames -----
+dirCon_stack = dir(stackpath);
+disp(['working in ', stackpath]); %Sanity Check
+expr='.*_w\d+.*_s\d+.*_t.*\.tif';
+Temp=cell([1,length(dirCon_stack)]); %Initialize cell array
+% ----- Identify the legitimate stacks -----
+i=1;
+for j=1:length(dirCon_stack)
+    Temp2=regexp(dirCon_stack(j).name,expr,'match','once','ignorecase');
+    if Temp2
+        Temp{i}=Temp2;
+        i=i+1;
+    end
+end
+% ----- Remove empty cells -----
+Temp(i:end)=[];
+% for j=length(Temp):-1:1
+%     if isempty(Temp{j})
+%         Temp(j)=[];
+%     end
+% end
+stacknames=Temp;
+dirCon_ff = dir(ffpath);
+end
+
