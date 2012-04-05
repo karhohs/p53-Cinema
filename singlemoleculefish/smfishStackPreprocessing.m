7cfde5bad132c930cd8482a1af476483b7012c97
Kyle Karhohs <gandalfisarockstar@gmail.com>
2012-04-04 23:49:55 -0400

    Fixed a threshold bug. The autothreshold now does a good job at handling a double peaked background histogram shape.

--- singlemoleculefish/smfishStackPreprocessing.m
+++ singlemoleculefish/smfishStackPreprocessing.m
@@ -321,24 +321,88 @@ end
 end
 
 function [threshold,n,xout,n2,xout2] = triminthresh(A)
-%----- Approximate histogram as triangle -----
+%Calculate a few rank statistics (assumes A is already sorted)
+la = length(A);
+q1a = A(round(0.25*la)); %first quartile
+q2a = A(round(0.50*la));
+q3a = A(round(0.75*la)); %third quartile
+myIQRa = q3a-q1a;
+myCutoffa = 3*myIQRa+q2a;
 %Create the histogram
 [n,xout]=hist(A,100);
+plot(xout,n)
+%Use the triangle threshold for the initial guess
 ind = triangleThreshCore(n);
-temp = A>xout(ind);
-ind = find(temp,1,'first');
-clear temp;
-A = A(ind:end);
-[n2,xout2] = hist(A,100);
-n2 = smooth(n2);
-n2 = conv(n2,[0.5 0 -0.5],'same'); %the central difference derivative
-for i = 2:length(n2);
-    if (n2(i-1)<0 && n2(i)>=0) || (abs(n2(i-1))<=1);
+threshold = xout(ind);
+%Look for minimum change in the number of foci or when the change in foci
+%is less than 1.
+B = A(A>threshold);
+[n2,xout2] = hist(B,100);
+n2der = smooth(n2);
+n2der = conv(n2der,[0.5 0 -0.5],'same'); %the central difference derivative to find the min
+for i = 2:length(n2der);
+    if (n2der(i-1)<0 && n2der(i)>=0)
+        ind = i-1;
+        break
+    elseif (abs(n2der(i-1))<=1) && (n2(i-1) == 0 || n2(i-1) == 1 || n2(i-1) == 2)
         ind = i-1;
         break
     end
 end
 threshold = xout2(ind);
+logicStepCounter = 1;
+while logicStepCounter ~= 0
+    switch logicStepCounter
+        case 1
+            if threshold<myCutoffa
+                %Find the peak of the putative signal.
+                [~,putativeSignalPeakInd] = max(n2);
+                putativeSignalPeak = xout2(putativeSignalPeakInd);
+                logicStepCounter = 2;
+            else
+                break
+            end
+        case 2
+            if (putativeSignalPeak>myCutoffa) && (putativeSignalPeakInd>=2)
+                %If the peak is greater than the cutoff than go forward with
+                %the threshold search. Look for the min to the left of this peak.
+                %It will be the final threshold.
+                for i = putativeSignalPeakInd:-1:2;
+                    if (n2der(i-1)<0 && n2der(i)>=0)
+                        ind = i-1;
+                        break
+                    elseif (abs(n2der(i-1))<=1) && (n2(i-1) == 0 || n2(i-1) == 1 || n2(i-1) == 2)
+                        ind = i-1;
+                        break
+                    end
+                end
+                threshold = xout2(ind);
+                break
+            else
+                logicStepCounter = 3;
+            end
+        case 3
+            %Repeat the triangle threshold method
+            C = B(B>putativeSignalPeak);
+            [n3,xout3] = hist(C,100);
+            n3der = smooth(n3);
+            n3der = conv(n3der,[0.5 0 -0.5],'same'); %the central difference derivative to find the min
+            for i = 2:length(n3der);
+                if (n3der(i-1)<0 && n3der(i)>=0)
+                        ind = i-1;
+                        break
+                elseif (abs(n3der(i-1))<=1) && (n3(i-1) == 0 || n3(i-1) == 1 || n3(i-1) == 2)
+                        ind = i-1;
+                        break
+                end
+            end
+            threshold = xout3(ind);
+            break
+        otherwise
+            disp('If you see this message something went wrong during threshold calculation.');
+            break
+    end
+end
 end
 
 function [ind2] = triangleThreshCore(n)
