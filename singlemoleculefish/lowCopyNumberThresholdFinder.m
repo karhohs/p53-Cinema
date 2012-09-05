eb1f26432678b69388bcb764d23dc19944366c72
Kyle Karhohs <gandalfisarockstar@gmail.com>
2012-09-05 16:43:31 -0400

    updated the description

--- singlemoleculefish/lowCopyNumberThresholdFinder.m
+++ singlemoleculefish/lowCopyNumberThresholdFinder.m
@@ -1,21 +1,27 @@
 function [] = lowCopyNumberThresholdFinder(smfishpath,varargin)
     % [] = my_tiffStacker(path,positions,timepoints)
 % Input:
-% path: a char. The path to the folder that contains the raw TIFF images from
-% imageJ.
-% positions (optional): an array of positive integers. The integers in this array represent the
-% positions for which stacks will be created.
-% timepoints (optional): a cell containing arrays of positive integers or
-% an array of positive integers. If it is a cell, then it must contain an
-% array for each position. If it is an array, only these timepoints will be
-% taken for each position.
+% smfishpath: a char. The path to the folder that contains the output from
+% smfishStackPreprocessing().
+% manualthresh: a logical variable that when set to true will launch a
+% user-interface that guides the selection of a threshold.
 %
 % Output:
 % There is no direct argument output. Rather, a new folder will be created
-% that recomputes the content of the smfishStackPreprocessing
+% that recomputes the output of the smfishStackPreprocessing() function
+% with a universal threshold that was calculated using all of the data in
+% from the previously created output folder.
 %
 % Description:
-% 
+% The idea is that the automatic detection of foci in the case where the
+% number of foci per cell is very low, <5, can be improved by considering a
+% whole collection of images instead of just one (that may only have a few
+% cells to begin with). I have observed that many of the  
+% foci identified in low copy number situations are questionable, because
+% they are either very dim or located extra-cellularly.
+%
+% There will also be an option to choose the threshold manually.
+%
 % Other Notes:
 % 
 
