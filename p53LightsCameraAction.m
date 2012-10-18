03a2ca723e7a8cbca9d60cb463333d6e3ebed249
Kyle Karhohs <gandalfisarockstar@gmail.com>
2012-10-18 17:31:54 -0400

    first commit

---
+++ p53LightsCameraAction.m
@@ -0,0 +1,17 @@
+function [] = p53LightsCameraAction()
+
+%Assumption #1: the source images are stored as TIFFs in a
+%particular format.
+%Assumption #2: the output format is .mp4
+
+%Where are the source images for the movie?
+
+%What is the size of the desired output?
+widthOut = 1136; %iPhone5
+heightOut = 640; %iPhone5
+
+%Is there magnification of the source image?
+magnification = 
+
+function [p53Script] = p53Screenwriter()
+
