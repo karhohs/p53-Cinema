9039a551a87133c657235ddd8b8667c28f22f051
Kyle Karhohs <gandalfisarockstar@gmail.com>
2012-10-21 02:25:03 -0400

    now accepts parameter value structure.

--- createMetadata.m
+++ createMetadata.m
@@ -1,41 +1,34 @@
+function [] = createMetadata(p)
 docNode = com.mathworks.xml.XMLUtils.createDocument('metadata');
 metadata = docNode.getDocumentElement;
 %time of Acquisition
 timeOfAcquisition = docNode.createElement('timeOfAcquisition');
-timeOfAcquisitionText = '1293';
-timeOfAcquisition.appendChild(docNode.createTextNode(timeOfAcquisitionText));
+timeOfAcquisition.appendChild(docNode.createTextNode(p.timeOfAcquisitionText));
 metadata.appendChild(timeOfAcquisition);
 %timepoint
 timepoint = docNode.createElement('timepoint');
-timepointText = '3';
-timepoint.appendChild(docNode.createTextNode(timepointText));
+timepoint.appendChild(docNode.createTextNode(p.timepointText));
 metadata.appendChild(timepoint);
 %wavelength
 wavelength = docNode.createElement('wavelength');
 wavelengthType = docNode.createElement('type');
-wavelengthTypeText = 'YFP';
-wavelengthType.appendChild(docNode.createTextNode(wavelengthTypeText));
+wavelengthType.appendChild(docNode.createTextNode(p.wavelengthTypeText));
 wavelength.appendChild(wavelengthType);
-wavelengthExposure = docNode.createElement('exposure');
-wavelengthExposureText = '100ms';
-wavelengthExposure.appendChild(docNode.createTextNode(wavelengthExposureText));
-wavelength.appendChild(wavelengthExposure);
+% wavelengthExposure = docNode.createElement('exposure');
+% wavelengthExposure.appendChild(docNode.createTextNode(p.wavelengthExposureText));
+% wavelength.appendChild(wavelengthExposure);
 metadata.appendChild(wavelength);
 %position
 position = docNode.createElement('position');
-positionText = '2';
-position.appendChild(docNode.createTextNode(positionText));
+position.appendChild(docNode.createTextNode(p.positionText));
 metadata.appendChild(position);
 %z-slice
 zSlice = docNode.createElement('z-slice');
-zSliceText = '1';
-zSlice.appendChild(docNode.createTextNode(zSliceText));
+zSlice.appendChild(docNode.createTextNode(p.zSliceText));
 metadata.appendChild(zSlice);
 %label
 label = docNode.createElement('label');
-labelText = 'mcf7';
-label.appendChild(docNode.createTextNode(labelText));
+label.appendChild(docNode.createTextNode(p.labelText));
 metadata.appendChild(label);
 %write to a file
-filename = 'test.xml';
-xmlwrite(filename,docNode);
+xmlwrite(p.filename,docNode);
