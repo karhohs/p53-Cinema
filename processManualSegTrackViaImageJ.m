c4f6ded1a5fcb15ae5eaf5df22795b986a44b3d0
Kyle Karhohs <gandalfisarockstar@gmail.com>
2012-12-07 10:03:03 -0500

    added XML export.

--- processManualSegTrackViaImageJ.m
+++ processManualSegTrackViaImageJ.m
@@ -14,6 +14,7 @@ function [] = processManualSegTrackViaImageJ(logpath,stackpath,varargin)
 % comes from ImageJ measurements. The following measurements MUST be
 % included in addition to any others: "Center of Mass", "Fit Ellipse", and
 % "Stack Position".
+%% main function
 p = inputParser;
 p.addRequired('logpath', @(x)isdir(x));
 p.addRequired('stackpath', @(x)isdir(x));
@@ -40,6 +41,8 @@ end
 [linkmap,uniqueUOL] = findLinkMap(unitOfLife); %Connect the cell segments and output a unique representative cell from each cell genealogy.
 %smooth the signal
 uniqueUOL = smoothenedsignal(uniqueUOL);
+%export data into the XML format, because it is an open format
+exportCellData2XML(uniqueUOL,logpath);
 end
 
 function [log_txt,log_num,numberOfCells,dirConLogs] = loadImageJLogFiles(logpath,logFile)
@@ -448,7 +451,38 @@ end
 end
 
 function [uniqueUOL] = smoothenedsignal(uniqueUOL)
-for i=1:realrootnodeslength
+for i=1:length(uniqueUOL)
     uniqueUOL(i).meanIntensity = smooth(uniqueUOL(i).meanIntensity,5);
 end
+end
+
+function [] = exportCellData2XML(uniqueUOL,outpath)
+docNode = com.mathworks.xml.XMLUtils.createDocument('data');
+data = docNode.getDocumentElement;
+popStat = docNode.createElement('population_statistics');
+%create an array that contains every signal measurement
+allSignal = [];
+for i=1:length(uniqueUOL)
+    allSignal = cat(2,allSignal,uniqueUOL(i).meanIntensity');
+end
+popStat.setAttribute('max',num2str(max(allSignal)));
+popStat.setAttribute('min',num2str(min(allSignal)));
+popStat.setAttribute('mean',num2str(mean(allSignal)));
+popStat.setAttribute('std',num2str(std(allSignal)));
+data.appendChild(popStat);
+%fill the XML file with signal data for each cell
+for i=1:length(uniqueUOL)
+    newCell = docNode.createElement('cell');
+    newCell.setAttribute('id',num2str(i));
+    data.appendChild(newCell);
+    %add signal data for mean_intensity
+    signal = docNode.createElement('signal');
+    signal.setAttribute('type','mean_intensity');
+    %make data comma delimited
+    str = sprintf('%0.5e,',uniqueUOL(i).meanIntensity);
+    str(end) = []; %remove the extra comma at the end
+    signal.appendChild(docNode.createTextNode(str));
+    newCell.appendChild(signal);
+end
+xmlwrite(fullfile(outpath,'data.xml'),docNode);
 end
