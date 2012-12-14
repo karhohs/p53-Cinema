98da4cc07e2588b87f2b08ed0a8706f3fd2aea2f
Kyle Karhohs <gandalfisarockstar@gmail.com>
2012-12-14 14:53:58 -0500

    first commit

---
+++ dataVisualization/flatfieldHeatblockMap.m
@@ -0,0 +1,11 @@
+function [] = flatfieldHeatblockMap(path2IntensityImage)
+%Load the flatfield gain image
+
+%Divide it into blocks
+%I choose to have a 10 by 10 grid
+
+%Calculate the percent change from the median
+
+%Save this percentage, the (x,y) information of the block and the ratio
+%between the width and height of the block
+end
